89b624740bbb2c2adbe6e241f959f172f97ebf26
diasgt123 <diasgt123@gmail.com>
2023-11-03 21:43:56 +0530

    htmltemplates added

--- app.py
+++ app.py
@@ -2,16 +2,24 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
+from htmltemplates import user_template, bot_template
+
+class ChatOpenAI:
+    pass  # Define the ChatOpenAI class or import it from the correct module
+
+class ConversationBufferMemory:
+    pass  # Define the ConversationBufferMemory class or import it from the correct module
 
 def get_pdf_text(pdf_docs):
-    text=""
+    text = ""
     for pdf in pdf_docs:
-        reader=PdfReader(pdf)
+        reader = PdfReader(pdf)
         for page in reader.pages:
-            text+=page.extract_text()
+            text += page.extract_text()
     return text
+
 def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(
         separator="\n",
@@ -21,19 +29,16 @@ def get_text_chunks(text):
     )
     chunks = text_splitter.split_text(text)
     return chunks
-def get_conversation_chain(vectorstore):
-    llm = ChatOpenAI()
-    # llm = HuggingFaceHub(repo_id="google/asasfjkhdsfjk", model_kwargs={"temperature":jdf, "max_length":512})
 
-    memory = ConversationBufferMemory(
-        memory_key='chat_history', return_messages=True)
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()  # Initialize ChatOpenAI class (if not imported)
+    memory = ConversationBufferMemory()  # Initialize ConversationBufferMemory class (if not imported)
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
         retriever=vectorstore.as_retriever(),
         memory=memory
     )
     return conversation_chain
-
 
 def handle_userinput(user_question):
     response = st.session_state.conversation({'question': user_question})
@@ -46,38 +51,38 @@ def handle_userinput(user_question):
         else:
             st.write(bot_template.replace(
                 "{{MSG}}", message.content), unsafe_allow_html=True)
+
 def get_vector_store(text_chunks):
-   embeddings=HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
-   vector_store=FAISS.from_texts(texts=text_chunks,embeddings=embeddings)
-   return vector_store
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vector_store = FAISS.from_texts(texts=text_chunks, embeddings=embeddings)
+    return vector_store
 
 def main():
     load_dotenv()
     st.set_page_config(page_title='My Streamlit App', page_icon=':books:')
     
+    if 'conversation' not in st.session_state:
+        st.session_state.conversation = None
+
     st.header("Textchatapp :books: ")
-    st.text_input("Enter your question")
+    user_question = st.text_input("Enter your question")
 
     with st.sidebar:
-        st.subheader("your files")
-        pdf_docs=st.file_uploader("Upload your file", accept_multiple_files=True)
-        if st.button("Submit"):
-           with st.spinner("Processing your files"):
-                #getting the large string from all pdfs
-                raw_text=get_pdf_text(pdf_docs)
-                
-                #splitting the text into chunks
-                text_chunks=get_text_chunks(raw_text)
-                #st.write(text_chunks)
-                #getting the vector store
-                vector_store=get_vector_store(text_chunks)
-                 # create conversation chain
-                st.session_state.conversation = get_conversation_chain(
-                    vectorstore)
+        st.subheader("Your files")
+        pdf_docs = st.file_uploader("Upload your file", accept_multiple_files=True)
+        if st.button("Submit") and pdf_docs:
+            with st.spinner("Processing your files"):
+                # Getting the large string from all PDFs
+                raw_text = get_pdf_text(pdf_docs)
+                # Splitting the text into chunks
+                text_chunks = get_text_chunks(raw_text)
+                # Getting the vector store
+                vector_store = get_vector_store(text_chunks)
+                # Create conversation chain
+                st.session_state.conversation = get_conversation_chain(vector_store)
 
+    if user_question:
+        handle_userinput(user_question)
 
-
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
---
+++ htmltemplates.py
@@ -0,0 +1,36 @@
+
+user_template = """
+<div style="background-color: #DCF8C6; padding: 10px; border-radius: 10px; margin: 10px 0;">
+    <strong>User:</strong> {{MSG}}
+</div>
+"""
+
+bot_template = """
+<div style="background-color: #FFE156; padding: 10px; border-radius: 10px; margin: 10px 0;">
+    <strong>Bot:</strong> {{MSG}}
+</div>
+"""
+
+info_template = """
+<div style="background-color: #B3E0FF; padding: 10px; border-radius: 10px; margin: 10px 0;">
+    <strong>Info:</strong> {{MSG}}
+</div>
+"""
+
+success_template = """
+<div style="background-color: #D4EDDA; padding: 10px; border-radius: 10px; margin: 10px 0;">
+    <strong>Success:</strong> {{MSG}}
+</div>
+"""
+
+warning_template = """
+<div style="background-color: #FFE08C; padding: 10px; border-radius: 10px; margin: 10px 0;">
+    <strong>Warning:</strong> {{MSG}}
+</div>
+"""
+
+error_template = """
+<div style="background-color: #F8D7DA; padding: 10px; border-radius: 10px; margin: 10px 0;">
+    <strong>Error:</strong> {{MSG}}
+</div>
+"""
